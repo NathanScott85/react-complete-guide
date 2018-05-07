aaf710c752a369d370d61e3fb015c877e88b4598
NathanScott85 <nathanscott@Nathans-MacBook-Air.local>
2018-05-07 22:03:28 +0100

    Styling with Style Sheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Person.css';
 const person = (props) => {
 
     return (
-        <div>
+        <div className="Person">
             <p Onclick={props.click}>i'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
