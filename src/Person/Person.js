afd97106ff3673c5b794b6abd280e042bbd4e2b1
nathanscott85 <nathanscott@Nathans-MacBook-Air.local>
2018-05-07 16:53:08 +0100

    Added Math.floor & Math.random

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,6 +3,7 @@ const person = () => {
 
     return <p>i'm a person and I am {Math.floor(Math.random() * 30)} years old</p> 
     /* Wrapping text in {} brace's like above allows it to be shown as JS code rather than plain text*/
+    
 }
 
 export default person;
