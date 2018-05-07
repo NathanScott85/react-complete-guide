196c7a3356b773fb9750e45bb18aa23fed125424
nathanscott85 <nathanscott@Nathans-MacBook-Air.local>
2018-05-07 16:52:21 +0100

    Added Text

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react';
 const person = () => {
 
-    return <p>i'm a person</p>
+    return <p>i'm a person and I am {Math.floor(Math.random() * 30)} years old</p> 
+    /* Wrapping text in {} brace's like above allows it to be shown as JS code rather than plain text*/
 }
 
 export default person;
