585e03c5bd0d982e34d8a2a578723d4528677e1b
nathanscott85 <nathanscott@Nathans-MacBook-Air.local>
2018-05-07 17:47:18 +0100

    Added props.children

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am a react app</h1>
         <p>I am a p tag</p>
+        <p>--------</p>
         <Person  name="Max" age="24"/>
-        <Person name="Steve" age="29"/> My Hobbies include: Goat Racing <Person/>
+        <Person name="Steve" age="29"> My Hobbies include: Goat Racing </Person>
         <Person  name="Robert" age="31"/>
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,14 @@
 import React from 'react';
-const person = () => {
-
-    return <p>i'm a person and I am {Math.floor(Math.random() * 30)} years old</p> 
-    /* Wrapping text in {} brace's like above allows it to be shown as JS code rather than plain text*/
+const person = (props) => {
     
-}
+        return  (
+         <div>
+        <p>i'm {props.name} and I am {props.age} years old</p> 
+        <p>{props.children}</p>
+     </div>
+    )
+
+    
+};
 
 export default person;
