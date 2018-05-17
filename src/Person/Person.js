2c56384d8811f58738e4fa00800935ad236b070f
NathanScott85 <nathansfeely@gmail.com>
2018-05-17 18:55:24 +0100

    Fixed Type Error Causing Issue

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ const person = (props) => {
 
     return (
         <div className="Person">
-            <p Onclick={props.click}>i'm {props.name} and I am {props.age} years old</p>
+            <p onClick={props.click}>i'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
