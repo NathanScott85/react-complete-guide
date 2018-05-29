7bfbf31ffb2a8a2f9e1bb16d37cd9684e83461bb
NathanScott85 <nathansfeely@gmail.com>
2018-05-29 20:23:27 +0100

    A better project Structure & Cockpit files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+import Person from '../components/Persons/Person/Person';
 
 class App extends Component {
   state = {
@@ -56,11 +55,12 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             {/*The key always has to be on the outer element*/}
-            return <ErrorBoundary key={person.id}><Person
+            return <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name} 
               age={person.age}
-              changed={(event) => this.nameChangedHandler(event, person.id)}/>  </ErrorBoundary>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/> 
           })}
         </div>
       );    
