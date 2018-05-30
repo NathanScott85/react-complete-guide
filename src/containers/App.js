75ffef7a793f98a0863f9319b164e66e823e35a6
NathanScott85 <nathansfeely@gmail.com>
2018-05-30 21:16:19 +0100

    PureComponents

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     console.log('[App.js] Inside Constructor', props);
@@ -26,10 +26,11 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount()');
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('[Update App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return true;
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[Update App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   return nextState.showPersons !== this.state.persons ||
+  //   nextState.showPersons !== this.showPersons;
+  // }
 
   componentWillUpdate(nextProps, nextState) {
     console.log('[Update App.js] componentWillUpdate', nextProps, nextState);
@@ -98,6 +99,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+      <button onClick={()=>{this.setState({showPersons:true})}}>Show </button>
       <Cockpit 
        appTitle ={this.props.title}
       showPersons={this.state.showPersons}
