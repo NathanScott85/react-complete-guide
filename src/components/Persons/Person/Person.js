444e88ee2cf9058b9dd19643f32b5666c86b9caa
NathanScott85 <nathansfeely@gmail.com>
2018-05-30 19:58:39 +0100

    Converting Stateless to Stateful Components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-    
-    return ( 
-            /*assigning the style here 
-          will over ride the class setting by default 
-          css rules not because of Radium*/
+class Person extends Component {
+    render () {
+        return ( 
+           
         <div className={classes.Person}>
-            <p onClick={props.click}>i'm {props.name} and I am {props.age} years old</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
+            <p onClick={this.props.click}>i'm {this.props.name} and I am {this.props.age} years old</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
         </div>
-    )
+        )
+    }
+}
 
-
-};
-
-export default person;
+export default Person;
 
